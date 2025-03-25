42c15906bc1b49efbac8c1c03cf610e0496af564
Brendan <103078608+brendanlai01@users.noreply.github.com>
2025-03-24 20:39:00 -0400

    create constructor and functions for linkedList

---
+++ LinkedList Project/list.js
@@ -0,0 +1,47 @@
+
+export class linkedList {
+    constructor(head = null){
+        this.head = head;
+    }
+
+    append(value){
+
+    }
+
+    prepend(value){
+
+    }
+
+    size(){
+
+    }
+
+    head(){
+
+    }
+
+    tail(){
+
+    }
+
+    at(index){
+
+    }
+
+    pop(){
+
+    }
+
+    contains(value){
+
+    }
+
+    find(value){
+
+    }
+
+    toString(){ /** The format should be: ( value ) -> ( value ) -> ( value ) -> null */
+
+    }
+
+}
