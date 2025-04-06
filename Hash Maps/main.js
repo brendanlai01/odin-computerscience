5da3621e1627db0641c9ed3c44bfece286183d52
Brendan <103078608+brendanlai01@users.noreply.github.com>
2025-04-06 03:48:58 -0400

    create test commands for hash maps

--- Hash Maps/main.js
+++ Hash Maps/main.js
@@ -1,3 +1,26 @@
 import { Hash } from "./hashMap.js";
 
-const test = new HashMap();
+const test = new Hash();
+
+test.set('apple', 'red')
+test.set('banana', 'yellow')
+test.set('carrot', 'orange')
+test.set('dog', 'brown')
+test.set('elephant', 'gray')
+test.set('frog', 'green')
+test.set('grape', 'purple')
+test.set('hat', 'black')
+test.set('ice cream', 'white')
+test.set('jacket', 'blue')
+test.set('kite', 'pink')
+test.set('lion', 'golden')
+
+console.log(test);
+console.log(test.set('apple', 'redder'));
+console.log(test.length())
+console.log(test.entries())
+
+test.set('moon', 'silver')
+
+console.log(test.length());
+console.log(test.entries())
