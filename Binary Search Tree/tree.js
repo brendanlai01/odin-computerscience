2c0eac814c818e0e160f8d6703d986183962c759
Brendan <103078608+brendanlai01@users.noreply.github.com>
2025-04-15 19:44:27 -0400

    Move prettyPrint to inside Tree

--- Binary Search Tree/tree.js
+++ Binary Search Tree/tree.js
@@ -9,19 +9,20 @@ export class Tree{
 }
 
 
+    prettyPrint(node){
+        const prettyPrint = (node, prefix = "", isLeft = true) => {
+            if (node === null) {
+            return;
+            }
+            if (node.right !== null) {
+            prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+            }
+            console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+            if (node.left !== null) {
+            prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+            }
+        };
+    }
 
 
-function prettyPrint(node){
-    const prettyPrint = (node, prefix = "", isLeft = true) => {
-        if (node === null) {
-          return;
-        }
-        if (node.right !== null) {
-          prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
-        }
-        console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
-        if (node.left !== null) {
-          prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
-        }
-      };
 }
