07a38fe6dc608b858b36676be7d72e2e230ab187
Brendan <103078608+brendanlai01@users.noreply.github.com>
2025-04-15 19:44:37 -0400

    Create buildTree function

--- Binary Search Tree/tree.js
+++ Binary Search Tree/tree.js
@@ -3,11 +3,19 @@ export class Tree{
     constructor(root){
         this.root = this.buildTree([...new Set(array)].sort((a,b) => a - b));
     }
+    
+    buildTree(arr){
+        if (array.length === 0) return null;
 
+        let mid = Math.floor(arr.length / 2);
 
+        let root = new Node(arr[mid]);
 
-}
+        root.left = this.buildTree(array.slice(0, mid));
+        root.right = this.buildTree(array.slice(mid + 1));
 
+        return root;
+    }
 
     prettyPrint(node){
         const prettyPrint = (node, prefix = "", isLeft = true) => {
